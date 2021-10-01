499966a67a153a6bcebf6efc5290d66c4e26c3c1
Marc Llop <anarka84@gmail.com>
2021-10-01 13:50:15 +0200

    anexo2 a medias

--- anexoPractica2/index.js
+++ anexoPractica2/index.js
@@ -2,3 +2,11 @@ function formulario() {
   let dni = document.forms['form']['dni'].value;
   if (dni === '') alert('Es necesario rellenar el dni');
 }
+
+function sumar() {
+  let suma = 0;
+  suma += parseInt(document.getElementById('resultadoSuma').value);
+  console.log(suma);
+}
+
+function inicializar() {}
