78012f410a01d9e00123cf1238319b13094f7092
craM-Hub <anarka84@gmail.com>
2022-01-19 13:09:13 +0100

    ej5 mensaje error

--- jqueryAnexoPractica2/ej5/ej5.js
+++ jqueryAnexoPractica2/ej5/ej5.js
@@ -2,6 +2,6 @@ $(function () {
 	$('#test').click(function () {
 		$.getJSON('https://catfact.ninja/fact', (data) => {
 			$('#contenedor').html(data.fact);
-		});
+		}).fail((response) => $('#contenedor').html(response.status));
 	});
 });
