7794150f79b1faa62598159c95353d8696de347a
craM-Hub <anarka84@gmail.com>
2022-02-16 12:52:14 +0100

    corregido examen, vaya chorrada

--- examen jquery/js/ej2.js
+++ examen jquery/js/ej2.js
@@ -1,15 +1,25 @@
 $(function () {
 	$('#pekin').click(function () {
 		$('#formContainer').append(
-			"<form><input type='text' placeholder='Nombre' name='nombre' /><input type='text' placeholder='Correo' name='correo' /><input type='button' value='submit' id='enviar'></form>"
+			"<form><input type='text' id='nombre' placeholder='Nombre' name='nombre' /><input type='text' id='correo' placeholder='Correo' name='correo' /><input type='button' value='submit' id='enviar'></form>"
 		);
+		$('#enviar').click(function () {
+			$.ajax({
+				method: 'GET',
+				url: 'datos.php',
+				data: { nombre: $('#nombre').val(), correo: $('#correo').val() },
+				success: function (data) {
+					$('#contenedor').html(data);
+				},
+			});
+		});
 	});
 
-	$('#enviar').click(function () {
+	/* $('#enviar').click(function () {
 		$.get('datos.php', $('form').serialize(), (response) =>
 			$('#contenedor').html(response)
 		).fail((request) =>
 			$('#contenedor').html(request.status + ' Error inesperado')
 		);
-	});
+	}); */
 });
